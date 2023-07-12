bd4fe3582d5d20bccae590c96487d5dcb2811102
Ankit Dhungana <80185690+ankit-dhungana@users.noreply.github.com>
2023-07-11 23:26:16 -0500

    CSS button added and JS for password validation added.

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordInput = document.getElementById("password");
+  const confirmPasswordInput = document.getElementById("confirm-password");
+  const errorText = document.querySelector(".error-text");
+  console.log(errorText.textContent);
+
+  passwordInput.classList.add(".invalid");
+  confirmPasswordInput.classList.add(".invalid");
+
+  confirmPasswordInput.addEventListener("input", function () {
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+    errorText.style.display = "block";
+    if (
+      password === confirmPassword &&
+      password.length === confirmPassword.length &&
+      password.length >= 3
+    ) {
+      errorText.style.display = "none";
+      passwordInput.style.border = "2px solid var(--vaild-color)";
+      confirmPasswordInput.style.border = "2px solid var(--vaild-color)";
+    }
+  });
+});
